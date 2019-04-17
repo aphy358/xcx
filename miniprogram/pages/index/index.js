f9eae6270db09f9637dbde10eb47cefdff3c6408
Joker <chenhua385148231@126.com>
2019-04-17 09:43:03 +0800

    f

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,51 +3,58 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: '',
-    count: 1
+    bannerSwiper: [
+      { // <!--济州神话世界度假酒店--蓝鼎 193847   -->
+        hotelId: '193847',
+        img: 'http://image.jladmin.cn/real_1537842489044.jpg'
+      },
+      { // <!--迪士尼好莱坞酒店20190108 44153   -->
+        hotelId: '44153',
+        img: 'http://image.jladmin.cn/real_1546935858144.jpg'
+      },
+      { // <!--广州长隆香江酒店 254542   -->
+        hotelId: '254542',
+        img: 'http://image.jladmin.cn/real_1539249111131.jpg'
+      },
+      { // <!--广州长隆酒店 43846   -->
+        hotelId: '43846',
+        img: 'http://image.jladmin.cn/real_1526027431776.jpg'
+      },
+      { // <!--广州长隆熊猫酒店 200595   -->
+        hotelId: '200595',
+        img: 'http://image.jladmin.cn/real_1526027469157.jpg'
+      },
+      { // <!--珠海长隆横琴湾酒店 43798   -->
+        hotelId: '43798',
+        img: 'http://image.jladmin.cn/real_1535941858376.jpg'
+      },
+      { // <!--珠海长隆马戏酒店 43813-->
+        hotelId: '43813',
+        img: 'http://image.jladmin.cn/real_1535941914859.jpg'
+      },
+      { // <!--珠海长隆企鹅酒店 -->
+        hotelId: '38441',
+        img: 'http://image.jladmin.cn/real_1535941952124.jpg'
+      },
+      { // <!--珠海长隆迎海酒店公寓 -->
+        hotelId: '43823',
+        img: 'http://image.jladmin.cn/real_1535941993949.jpg'
+      },
+      { // <!--澳门金沙度假区-威尼斯人 -->
+        hotelId: '37976',
+        img: 'http://image.jladmin.cn/real_1539248861577.jpg'
+      },
+      { // <!--澳门喜来登酒店 -->
+        hotelId: '38900',
+        img: 'http://image.jladmin.cn/real_1539248810488.jpg'
+      },
+    ]
   },
 
   onReady: function() {
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
   },
   
 })
