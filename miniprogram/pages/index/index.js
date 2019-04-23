7ba743b9420a68a5134925a697d6a4137080d6d9
Joker <chenhua385148231@126.com>
2019-04-23 17:34:34 +0800

    f

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -12,34 +12,6 @@ Page({
         hotelId: '44153',
         img: 'http://image.jladmin.cn/real_1546935858144.jpg'
       },
-      { // <!--广州长隆香江酒店 254542   -->
-        hotelId: '254542',
-        img: 'http://image.jladmin.cn/real_1539249111131.jpg'
-      },
-      { // <!--广州长隆酒店 43846   -->
-        hotelId: '43846',
-        img: 'http://image.jladmin.cn/real_1526027431776.jpg'
-      },
-      { // <!--广州长隆熊猫酒店 200595   -->
-        hotelId: '200595',
-        img: 'http://image.jladmin.cn/real_1526027469157.jpg'
-      },
-      { // <!--珠海长隆横琴湾酒店 43798   -->
-        hotelId: '43798',
-        img: 'http://image.jladmin.cn/real_1535941858376.jpg'
-      },
-      { // <!--珠海长隆马戏酒店 43813-->
-        hotelId: '43813',
-        img: 'http://image.jladmin.cn/real_1535941914859.jpg'
-      },
-      { // <!--珠海长隆企鹅酒店 -->
-        hotelId: '38441',
-        img: 'http://image.jladmin.cn/real_1535941952124.jpg'
-      },
-      { // <!--珠海长隆迎海酒店公寓 -->
-        hotelId: '43823',
-        img: 'http://image.jladmin.cn/real_1535941993949.jpg'
-      },
       { // <!--澳门金沙度假区-威尼斯人 -->
         hotelId: '37976',
         img: 'http://image.jladmin.cn/real_1539248861577.jpg'
