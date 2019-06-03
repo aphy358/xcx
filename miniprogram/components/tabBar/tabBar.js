eac8cb1ede80471587e93ae3f9b1831481b57ce1
Joker <chenhua385148231@126.com>
2019-06-03 18:41:36 +0800

    f

--- miniprogram/components/tabBar/tabBar.js
+++ miniprogram/components/tabBar/tabBar.js
@@ -4,14 +4,51 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    index: {
+      type: String,
+      value: 1
+    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    tabList: [
+      {
+        tabIndex: 1,
+        tabIcon: "icon-home-page",
+        tabText: "首页",
+        isActive: true
+      },
+      {
+        tabIndex: 2,
+        tabIcon: "icon-lipin",
+        tabText: "福利社",
+        isActive: false
+      },
+      {
+        tabIndex: 3,
+        tabIcon: "icon-my-center",
+        tabText: "我的",
+        isActive: false
+      },
+    ]
+  },
 
+  lifetimes: {
+    attached: function () {
+      var tmpList = this.data.tabList
+      for (var i = 0; i < tmpList.length; i++){
+        tmpList[i].isActive = tmpList[i].tabIndex == this.data.index
+      }
+
+      this.setData({
+        tabList: tmpList
+      })
+    },
+    detached: function () {
+    },
   },
 
   /**
