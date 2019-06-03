d4d52d016157fc35c4e8dda284c0b9577e28fbe6
Tsingwu <1211751992@qq.com>
2019-06-03 10:22:36 +0800

    福利社

--- miniprogram/pages/welfareStore/welfareStore.js
+++ miniprogram/pages/welfareStore/welfareStore.js
@@ -55,21 +55,12 @@ Page({
   },
   
   onPageScroll(e){
-    let topHeight =  520 * this.data.bannerWidth / 375;
-    let botHeight = ((this.data.bannerWidth + 30) * 1000)/750;
-    console.log(topHeight);
-    // console.log(botHeight);
-    if (e.scrollTop >= topHeight){
+    let imgH =  480 * this.data.bannerWidth / 375;
+    if (e.scrollTop >= imgH){
       this.setData({
         titleClass: ' title-fixed'
       })
-    }
-    // else{
-    //   this.setData({
-    //     titleClass: ''
-    //   })
-    // }
-    if (e.scrollTop <= botHeight){
+    }else{
       this.setData({
         titleClass: ''
       })
