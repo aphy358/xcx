c3f4f4a3f336587c8cd53ae76f54babf3d68bf85
aphy358 <358937572@qq.com>
2019-06-07 13:59:27 +0800

    f

--- miniprogram/pages/welfareStore/welfareStore.js
+++ miniprogram/pages/welfareStore/welfareStore.js
@@ -51,7 +51,10 @@ Page({
       }
     ],
     titleClass: '',
-    showNavBarTitle: false
+    showNavBarTitle: false,
+
+    showShareSelector: false,
+    showShareImg: false
   },
   
   onPageScroll(e){
@@ -80,4 +83,46 @@ Page({
   },
   onReady: function () {
   },
+
+
+  gotoTicketDetail(e) {
+    wx.navigateTo({
+      url: '/pages/ticketDetail/ticketDetail',
+    })
+  },
+  
+  // 显示分享方式的选择底部弹窗
+  showShare() {
+    wx.hideTabBar()
+    this.setData({
+      showShareSelector: true
+    })
+  },
+  // 隐藏分享方式的选择底部弹窗
+  hideSelector() {
+    this.setData({
+      showShareSelector: false
+    })
+
+    setTimeout(function () {
+      wx.showTabBar()
+    }, 300)
+  },
+  // 显示生成的海报
+  showSImg() {
+    this.setData({
+      showShareImg: true,
+      showShareSelector: false
+    })
+    wx.hideTabBar()
+  },
+  // 隐藏海报
+  hideImage() {
+    this.setData({
+      showShareImg: false
+    })
+    wx.showTabBar()
+  },
+
+
 });
